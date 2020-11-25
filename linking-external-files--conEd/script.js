e91db3a1c378550ac191a21c6cc6bdce3c5b1997
Paul <paulandrews810@gmail.com>
2020-11-25 12:43:08 -0500

    adding more months of work to the repo. Includes mentoring exercises that i've worked on one on one with students, advanced js exercises, and the start of new typescript course i'm taking

---
+++ linking-external-files--conEd/script.js
@@ -0,0 +1,5 @@
+console.log(`Amazing job! Now you can connect your script files to your html!`)
+
+const typeOfCheck = typeof 2;
+
+console.log(typeOfCheck);
---
+++ script.js
@@ -0,0 +1,8 @@
+export const wizard = 'harry potter';
+export const darkWizard = 'voldemort';
+
+const castASpell = () => {
+    console.log('a spell has been cast');
+}
+
+export default castASpell;
